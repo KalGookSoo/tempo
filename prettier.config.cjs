9de57c34b59087dbfa8a77dc0447f9447249b66a
Doyevskyi <ga.miro3721@gmail.com>
2026-07-09 20:26:51 +0900

    chore: prettier dependency 추가

---
+++ prettier.config.cjs
@@ -0,0 +1,35 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  endOfLine: 'lf',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none',
+  arrowParens: 'always',
+  bracketSpacing: true,
+  plugins: [
+    '@ianvs/prettier-plugin-sort-imports',
+    'prettier-plugin-tailwindcss'
+  ],
+  importOrder: [
+    '<BUILTIN_MODULES>',
+    '',
+    '^(react/(.*)$)|^(react$)',
+    '^(next/(.*)$)|^(next$)',
+    '<THIRD_PARTY_MODULES>',
+    '',
+    '^types$',
+    '^@/types/(.*)$',
+    '^@/config/(.*)$',
+    '^@/lib/(.*)$',
+    '^@/hooks/(.*)$',
+    '^@/components/ui/(.*)$',
+    '^@/components/(.*)$',
+    '^@/registry/(.*)$',
+    '^@/styles/(.*)$',
+    '^@/app/(.*)$',
+    '',
+    '^[./]'
+  ],
+  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy']
+};
